ac60027b80a67ae5f1312ded98ed4871a19abfd8
Micah Blu <micahblu@gmail.com>
2013-08-04 22:39:40 -0700

    added pagination

--- archive.php
+++ archive.php
@@ -44,7 +44,7 @@ get_header(); ?>
 	<?php else : ?>
 		<?php get_template_part( 'content', 'none' ); ?>
 	<?php endif; ?>
-
+	
 	</div><!-- .large-8 .columns -->
 	
 	<div class="large-4 columns">
--- content.php
+++ content.php
@@ -66,3 +66,16 @@
 			<?php endif; ?>
 		</footer><!-- .entry-meta -->
 	</article><!-- #post -->
+	
+	<?php
+	global $wp_query;
+
+	$big = 999999999; // need an unlikely integer
+	
+	echo paginate_links( array(
+		'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+		'format' => '?paged=%#%',
+		'current' => max( 1, get_query_var('paged') ),
+		'total' => $wp_query->max_num_pages
+	) );
+	?>
