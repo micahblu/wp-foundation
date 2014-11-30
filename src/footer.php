8ed062d9f26b952244f921f841276de05c693d8e
Micah Blu <micahblu@gmail.com>
2014-11-30 11:25:46 -0800

    Added WP Foundation link to footer

--- src/footer.php
+++ src/footer.php
@@ -12,9 +12,9 @@
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
 		<div class="site-info large-12 columns">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp-foundation' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wp-foundation' ), 'WordPress' ); ?></a>
+			<a target="_blank" href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp-foundation' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wp-foundation' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s.', 'wp-foundation' ), 'wp-foundation'); ?>
+			<a target="_blank" href="https://github.com/micahblu/wp-foundation"><?php printf( __( 'Theme: %1$s.', 'wp-foundation' ), 'WP Fgitoundation'); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
