227186ee249cae37582c5cd3e31ec0c1c3e92efc
Micah Blu <micahblu@gmail.com>
2014-11-30 11:35:04 -0800

    Made footer credits less verbose;

--- src/footer.php
+++ src/footer.php
@@ -12,9 +12,9 @@
 
 	<footer id="colophon" class="site-footer row" role="contentinfo">
 		<div class="site-info large-12 columns">
-			<a target="_blank" href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp-foundation' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'wp-foundation' ), 'WordPress' ); ?></a>
+			<a target="_blank" href="<?php echo esc_url( __( 'http://wordpress.org/', 'wp-foundation' ) ); ?>"><?php printf( __( 'Powered by %s', 'wp-foundation' ), 'WordPress' ); ?></a>
 			<span class="sep"> and </span>
-			<a target="_blank" href="https://github.com/micahblu/wp-foundation"><?php printf( __( 'Theme: %1$s.', 'wp-foundation' ), 'WP Fgitoundation'); ?></a>
+			<a target="_blank" href="https://github.com/micahblu/wp-foundation"><?php printf( __( '%1$s.', 'wp-foundation' ), 'WP Foundation'); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
